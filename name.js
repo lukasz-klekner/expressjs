91cb6ed23dcfff05e54a2b26389545a560ad20f8
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-20 17:36:57 +0100

    fix: write name in file and read name from file

--- name.js
+++ name.js
@@ -1,19 +1,27 @@
 const express = require('express')
+const { readFile, writeFile } = require('fs').promises
+
 
 const app = express()
 let NAME = ''
 
-app.get('/name/show', (req,res) => {
-    res.send(`Name: ${NAME}`)
+app.get('/name/show', async (req,res) => {
+    res.send(`Name: ${await readFile('name.txt', 'utf-8')}`)
 })
 
-app.get('/name/check', (req,res) => {
-    res.send(`Name: ${NAME ? 'TAK' : 'NIE'}`)
+app.get('/name/check', async (req,res) => {
+    try {
+        await readFile('name.txt', 'utf-8')
+        res.send('There is a name')
+    } catch(e) {
+        res.send('There is no name')
+    }
 })
 
-app.get('/name/set/:name', (req,res) => {
+app.get('/name/set/:name', async (req,res) => {
     const { name } = req.params
     NAME = name
+    await writeFile('./name.txt', NAME, 'utf-8')
     res.send(`Name: ${NAME}`)
 })
 
